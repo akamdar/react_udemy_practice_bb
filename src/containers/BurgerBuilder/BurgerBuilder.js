ba74c0245b4d0ef0e18337677bb84ead52f30c05
Ankit <ankit.kamdar89@gmail.com>
2020-03-04 00:28:53 +0530

    Commit changes for the forms and redux thunk

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,24 +8,16 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
-import * as actionTypes from '../../store/actions';
+import * as actions from '../../store/actions/index';
 
 class BurgerBuilder extends Component {
 
   state = {
-    purchasing: false,
-    loading: false,
-    error: false
+    purchasing: false
   }
 
   componentDidMount() {
-    // axios.get('/ingredients.json')
-    //   .then(response => {
-    //     this.setState({ ingredients: response.data });
-    //   })
-    //   .catch( error => {
-    //     this.setState({error: true});
-    //   });
+    this.props.onInitIngredient();
   }
 
   updatePurchaseState (ingredients) {
@@ -45,6 +37,7 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
+    this.props.onInitPurchase();
     this.props.history.push('/checkout');
   }
 
@@ -56,7 +49,7 @@ class BurgerBuilder extends Component {
       disabledInfo[key] = disabledInfo[key] <= 0;
     }
     let orderSummary = null; 
-    let burger = this.state.error ? <p style={{textAlign: 'center'}}>Ingredients can't be loaded</p> : <Spinner />;
+    let burger = this.props.error ? <p style={{textAlign: 'center'}}>Ingredients can't be loaded</p> : <Spinner />;
     
     if ( this.props.ings ) {
       burger = (
@@ -78,10 +71,6 @@ class BurgerBuilder extends Component {
         price={this.props.totalPrice}/>
     }
 
-    if ( this.state.loading ) {
-      orderSummary = <Spinner />
-    }
-
     return (
       <Fragment>
         <Modal show={this.state.purchasing} modalCanceled={this.purchaseCancelHandler}>
@@ -95,15 +84,18 @@ class BurgerBuilder extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients,
-    totalPrice: state.totalPrice
+    ings: state.burgerBuilder.ingredients,
+    totalPrice: state.burgerBuilder.totalPrice,
+    error: state.burgerBuilder.error
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onIngredientAdded: (ingName) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
-    onIngredientRemoved: (ingName) => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName})
+    onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
+    onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
+    onInitIngredient: () => dispatch(actions.initIngredients()),
+    onInitPurchase: () => dispatch(actions.purchaseInit())
   }
 }
 
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import * as actions from '../../store/actions/index';
 class Orders extends Component {
 
     state = {
@@ -12,27 +14,13 @@ class Orders extends Component {
     }
 
     componentDidMount() {
-        axios.get('/order.json')
-            .then(response => {
-                console.log(response.data);
-                const fetchedOrders = [];
-                for (let key in response.data) {
-                    fetchedOrders.push({
-                        ...response.data[key],
-                        id: key
-                    });
-                }
-                this.setState({ loading: false,  orders: fetchedOrders });
-            })
-            .catch(err => {
-                this.setState({ loading: false });
-            })
+        this.props.onFetchOrders();
     }
 
     render() {
         let orders = <Spinner />;
-        if ( !this.state.loading ) {
-            orders = this.state.orders.map(order => (
+        if ( !this.props.loading ) {
+            orders = this.props.orders.map(order => (
                 <Order 
                     key={order.id} 
                     ingredients={order.ingredients}
@@ -47,4 +35,17 @@ class Orders extends Component {
     }
 }
 
-export default withErrorHandler(Orders, axios);
+const mapStateToProps = state => {
+    return {
+        orders: state.order.orders,
+        loading: state.order.loading
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onFetchOrders: () => dispatch(actions.fetchOrders())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
